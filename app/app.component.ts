3274bc1f5bf2cf40e1440e8b2538c50e27832c82
Ryan <ryanjonesirl@gmail.com>
2017-05-02 10:26:47 -0700

    add a custom class to increase the size of the stars

--- app/app.component.ts
+++ app/app.component.ts
@@ -26,22 +26,22 @@ import { Component } from '@angular/core';
     <div class="well">
       <h3>Challenge: Build a favorite star</h3>
       <h6>First star, changes color on click</h6>
-      <span [class.change-color]="isClassVisible" (click)="isClassVisible = !isClassVisible" class="glyphicon glyphicon-star-empty"></span>
+      <span [class.change-color]="isClassVisible" (click)="isClassVisible = !isClassVisible" class="glyphicon glyphicon-star-empty gi-2x"></span>
       <h6>Second star, fills center on click</h6>
-      <span (click)="starClass = 'glyphicon glyphicon-star'" class="{{ starClass }}"></span>
+      <span (click)="starClass = 'glyphicon glyphicon-star gi-2x'" class="{{ starClass }}"></span>
       <h6>Third star, fills and emptys on click</h6>
       <span (click)="isStarFilled = !isStarFilled" [class]="fillStar(isStarFilled)"></span>
     </div>
   `,
   styles: [
   `
-  .change-color {
-    color: red;
-  }
+  .change-color { color: red; }
+  .gi-2x { font-size: 2em; }
+
   `]
 })
 export class AppComponent {
-  starClass = "glyphicon glyphicon-star-empty";
+  starClass = "glyphicon glyphicon-star-empty gi-2x";
   title = "Created using property binding and interporlation";
   title2 = "Angular App";
   imageURL = "http://www.jqueryscript.net/images/Simplest-Responsive-jQuery-Image-Lightbox-Plugin-simple-lightbox.jpg";
@@ -57,9 +57,9 @@ export class AppComponent {
 
   fillStar(isStarFilled) {
     if(isStarFilled) {
-      return "glyphicon glyphicon-star";
+      return "glyphicon glyphicon-star gi-2x";
     } else {
-      return "glyphicon glyphicon-star-empty";
+      return "glyphicon glyphicon-star-empty gi-2x";
     }
   }
 
